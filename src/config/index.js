ed606f1d6cb0b1b06581108220a39cdc14c1c752
mycok <awesomeme155@gmail.com>
2019-10-27 00:46:01 +0300

    ch:(setup) initial project setup
    - Setup a node.js application
    - Add a package.json file with all the required initial dependencies
    - Setup the project folder structure
    - Add a babel config file
    - Add a nodemon config file
    - Add a docker config file
    - Add an eslint config file
    - Add a mocharc config file
    - Add nycrc config file
    - Add a travis config file
    - Add a swagger config file
    - Add a webpack config file
    
    [Delivers #2345678]

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 4000,
+  jwtSecret: process.env.JWT_SECRET || 'somerandomstring',
+};
+
+export default config;
---
+++ src/server/express.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../../swagger.json';
+
+const app = express();
+
+app.use(compress());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors());
+app.set('strict routing', true);
+
+app.get('/', (req, res) => {
+  res.redirect('/swagger');
+});
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default app;
